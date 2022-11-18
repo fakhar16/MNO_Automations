16f6e0e0c55ba227023c55f8177925b234fc700d
fakhar.ahmad <[MNOSOLTNS-10310]>
2022-11-18 11:41:22 -0500

    webhook logic update

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 @app.route('/git_update', methods=['POST'])
 def git_update():
     if request.method == 'POST':
-        repo = git.Repo('./MNO_Automations')
+        repo = git.Repo('https://github.com/fakhar16/MNO_Automations')
         origin = repo.remotes.origin
-        repo.create_head('master',
-                         origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+        # repo.create_head('master',
+        #                  origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
         origin.pull()
-        return '', 200
+        return 'Updated PythonAnywhere successfully', 200
     else:
-        return '', 400
+        return 'Wrong event type', 400
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
