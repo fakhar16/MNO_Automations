a32b72f63289ebd5108d82a65d87998b97e5f66f
fakhar.ahmad <[MNOSOLTNS-10310]>
2022-11-18 10:29:58 -0500

    Adding auto update from gitHub code

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-import webbrowser
-from flask import *
+from flask import Flask, render_template
+import git
 
 app = Flask(__name__ )
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.repo('./MNO_Automations')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("index.html")
-
-def open_browser():
-    webbrowser.open_new('http://127.0.0.1:5000/')
-
-if __name__=='__main__':
-    open_browser()
-    app.run(port=5000)
