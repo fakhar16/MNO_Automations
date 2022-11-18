dffddcd783cd0cdada1b3922258d7f4b64375187
fakhar.ahmad <[MNOSOLTNS-10310]>
2022-11-18 15:27:40 -0500

    new logic for webhook

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import git
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['GET', 'POST'])
 def git_update():
     if request.method == 'POST':
         repo = git.Repo('./MNO_Automations')
         origin = repo.remotes.origin
         repo.create_head('main',
-                         origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+                         origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
 
